9cb49735cc0c6ccabc4c11b9963b1003221e040d
vloss <viniciusvianaloss@gmail.com>
2023-01-11 10:21:06 -0300

    Iniciando projeto

---
+++ Ciclo de Vida/Ciclo de Vida/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ciclo de Vida
+//
+//  Created by Vinicius Loss on 11/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
